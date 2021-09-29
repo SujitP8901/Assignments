29c1e4b920151d77c3d7ed01af1a8fdae3666fda
SujitP8901 <91279528+SujitP8901@users.noreply.github.com>
2021-09-29 23:06:15 +0530

    Update A14_Q1.c

--- Assignment 14-15/A14_Q1.c
+++ Assignment 14-15/A14_Q1.c
@@ -1,6 +1,6 @@
 //	Accept number of rows and number of columns from user and display below pattern.
 //	Input : iRow = 4 iCol = 4
-//	Output : *
+//	Output :         *
 //			 * *
 //			 * * *
 //			 * * * *
